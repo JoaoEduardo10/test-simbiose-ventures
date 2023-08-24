9c91ee6ebd54dc9b6e78c7595f1b3dcf83f74a9c
João Eduardo <joaoeduardoj920@gmail.com>
2023-08-24 18:14:33 -0300

    feat(get-person): created middleware

---
+++ back-end/src/server/middlewares/get-person/get-person.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { Person } from "../../models/Person";
+import { Not_Fould } from "../../errors/api-errors";
+
+class GetPersonMiddleware {
+  async middleware(
+    req: Request<{ id: string }>,
+    _res: Response,
+    next: NextFunction
+  ) {
+    const { id } = req.params;
+
+    const person = await Person.findById(id);
+
+    if (!person) {
+      throw new Not_Fould("id invalido!");
+    }
+
+    next();
+  }
+}
+
+export { GetPersonMiddleware };
