0ef0f7a20128d9fd454a99c6b0235dbd447cf88b
João Eduardo <joaoeduardoj920@gmail.com>
2023-08-26 13:51:22 -0300

    feat(swagger): added to api documentation

--- back-end/src/server/server.ts
+++ back-end/src/server/server.ts
@@ -2,10 +2,21 @@ import "express-async-errors";
 import express from "express";
 import { globalsErrors } from "./middlewares/globals-errors";
 import { router } from "./router";
+import swagger from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
+import cors from "cors";
 
 const server = express();
 
+server.use(cors());
+
 server.use(express.json());
+
+server.use(
+  "/simbiose/documentation",
+  swagger.serve,
+  swagger.setup(swaggerDocs)
+);
 
 server.use("/v1", router);
 
